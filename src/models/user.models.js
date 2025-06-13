30134cc2e832ce1c12a183596521642936759b10
TEJAS MISHRA <tejasmishra040907@gmail.com>
2025-06-13 18:29:44 +0530

    platform setup done

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -67,7 +67,7 @@ userSchema.methods.generateAccessToken = function(){
             _id: this._id,
             email: this.email,
             username: this.username,
-            fullName: thia.fullName
+            fullName: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
